b6cafdec00f5bf37a15ef22d91f6980f4793e56b
Albert <albert.bayazidi@gmail.com>
2024-06-09 22:31:38 +0200

    passer på å sende returnere nok divs avhenig av hvor stor bobla er

--- src/Components/Sub-components/Event-kalender-button.jsx
+++ src/Components/Sub-components/Event-kalender-button.jsx
@@ -8,14 +8,19 @@ function EventButton() {
     }
 
     return (
-        <button class="grid grid-rows-subgrid row-start-2 row-span-3 bg-second rounded-3xl outline-none hover:bg-third"
+        <button class="grid grid-rows-subgrid row-start-2 row-span-3 bg-main_green rounded-3xl outline-none hover:bg-hover_green group"
         value = "2" onClick = {handleButtonClick}>
             <div>
-                <img src="/plus_cir.svg" alt="temp_pluss" class="bg-white rounded-full size-10 ml-auto mt-5 mr-3" />
+                <svg class="size-16 fill-current text-white ml-auto stroke-main_green group-hover:stroke-hover_green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
             </div>
 
             <div >
-                <img src="https://www.svgrepo.com/show/35457/calendar-symbol.svg" class="size-36 ml-10 mt-10" />
+            <svg class="size-24 fill-none stroke-main_gray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+            </svg>
+
             </div>
 
             <div class = "row-start-3 h-full">
--- src/Components/Sub-components/simple-button.jsx
+++ src/Components/Sub-components/simple-button.jsx
@@ -1,11 +1,36 @@
 function SimpleButton(obj){
-    return (            
-    <a class={obj.row_loc + " " + obj.col_loc + " " + obj.span + " " 
-    + "bg-second rounded-3xl shadow-3xl hover:bg-third"} 
-    href={obj.link} target="_blank">
-        <img src = "/plus_cir.svg" class = "bg-white rounded-full size-10 " alt = "pluss icon"/>
-        <img src = {obj.img} class = {obj.img_style} alt = "figur"/>
-        <span class = "text-white text-4xl">{obj.text}</span>
-    </a>)
+    if (obj.span === "row-span-2"){
+        return (            
+            <a class={obj.row_loc + " " + obj.col_loc + " " + obj.span + " " 
+            + "grid grid-rows-subgrid bg-main_green rounded-3xl shadow-3xl hover:bg-hover_green group"} 
+                href={obj.link} target="_blank">
+
+                <div>
+
+                <svg class="size-16 fill-current text-white ml-auto stroke-main_green group-hover:stroke-hover_green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
+                <img src = {obj.img} class = {obj.img_style} alt = "figur"/>
+                <span class = "text-white text-4xl">{obj.text}</span>
+                </div>
+
+            </a>)
+    }
+    else{
+        return (            
+        <a class={obj.row_loc + " " + obj.col_loc + " " + obj.span + " " 
+        + " bg-main_green rounded-3xl shadow-3xl hover:bg-hover_green group"} 
+            href={obj.link} target="_blank">
+
+            <svg class="size-16 fill-current text-white ml-auto stroke-main_green group-hover:stroke-hover_green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+
+            
+            <img src = {obj.img} class = {obj.img_style} alt = "figur"/>
+            <span class = "text-white text-4xl">{obj.text}</span>
+        </a>)
+    }
 }
 export default SimpleButton;
