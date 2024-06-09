92e2ebbb2180bae402f57646830f37804c8c9776
Albert <albert.bayazidi@gmail.com>
2024-06-09 13:29:11 +0200

    omksriving av div for tekst element for å prøve å fikse tekst plaseringen

--- src/Components/Sub-components/Event-kalender-button.jsx
+++ src/Components/Sub-components/Event-kalender-button.jsx
@@ -8,7 +8,7 @@ function EventButton() {
     }
 
     return (
-        <button className="grid grid-rows-subgrid row-start-2 row-span-3 bg-second rounded-3xl outline-none hover:bg-third"
+        <button class="grid grid-rows-subgrid row-start-2 row-span-3 bg-second rounded-3xl outline-none hover:bg-third"
         value = "2" onClick = {handleButtonClick}>
             <div>
                 <img src="/plus_cir.svg" alt="temp_pluss" class="bg-white rounded-full size-10 ml-auto mt-5 mr-3" />
@@ -18,8 +18,8 @@ function EventButton() {
                 <img src="https://www.svgrepo.com/show/35457/calendar-symbol.svg" class="size-36 ml-10 mt-10" />
             </div>
 
-            <div class = "row-start-3">
-                <span className="text-white text-4xl">
+            <div class = "row-start-3 h-full">
+                <span class="text-white text-4xl ">
                     Eventkalender
                 </span>
             </div>
