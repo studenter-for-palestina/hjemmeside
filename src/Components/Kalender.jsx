43885e84ce1e6c3bac43fe05fd9772a82749b094
Albert <albert.bayazidi@gmail.com>
2024-06-12 15:40:46 +0200

    begynt på omoss og ressurs side, la til funksjonalitete til complex knapp da det manglet, og formateringing

--- src/Components/Kalender.jsx
+++ src/Components/Kalender.jsx
@@ -2,18 +2,33 @@ import BackButton from './Sub-components/back-button';
 import { Fragment } from "react";
 
 function Kalender(obj){
+    function kalenderEvent(event){
+        return (
+            <div class = " p-4 m-4">
+                <div class = "flex">
+                    <h1 class = "text-2xl font-bold ">{event.title}</h1>
+                    <h1 class = "text-2xl font-bold underline px-2">{"" + event.date + " kl " + event.time }</h1>
+                    <h1 class = "text-2xl px-2"> { " | " }</h1>
+                    <h1 class = "text-2xl text-main_orange px-2"> {  event.location}</h1>
+                </div>
+                
+                <p class = "text-main_green text-ellipsis overflow-hidden ">{event.description}</p>
+                <a href = {event.link} class = "">Link til arragment</a>
+            </div>
+        )
+    }
     return (
         <Fragment>
             <div class = "sm:grid sm:grid-cols-2 sm:gap-4 h-screen">
                 <div class = "h-full">
                     <BackButton />
+                    <svg class="size-96 fill-none mx-auto my-32 stroke-main_gray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                    </svg>
 
                 </div>
-                <div class = "h-full grid grid-cols-1 divide-y-2">
-                    <div>hei</div>
-                    <div>hei</div>
-                    <div>hei</div>
-                    <div>hei</div>
+                <div class = "h-full ml-10 w-3/4 grid grid-cols-1 divide-y-2 divide-black overflow-y-auto">
+                    {obj.events.map(event => kalenderEvent(event))}
                 </div>
         
 
---
+++ src/Components/OmOss.jsx
@@ -0,0 +1,11 @@
+import BackButton from './Sub-components/back-button';
+
+function OmOss(){
+    return(
+        <div>
+            <BackButton />
+            <h1>Om oss</h1>
+        </div>
+    )
+}
+export default OmOss; 
