31d2c2bb9bb439c3c186d7c3bc97160b228b1e5f
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2024-06-26 11:45:49 +0200

    :wrench: Variabler puttes i config-fil #1
    Signed-off-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

---
+++ config.js
@@ -0,0 +1,40 @@
+export default {
+    LENKER: {
+        MEDLEM_LINK: "https://docs.google.com/forms/d/e/1FAIpQLScKfgm3Pt80zE9Ll6ogfbUeJRqd0Wzdt74jnPrxzgahOFHr6g/viewform",
+        BY: "Trondheim", // må endres i `./index.html` også
+        SPLEIS_LINK: "https://www.spleis.no/project/371936",
+        DISK_LINK: "https://drive.google.com/drive/folders/12IzO-MJ8YODd933nfj6TT1PiTv7eesl7?usp=sharing",
+        LINK_TREE: "https://linktr.ee/Studenterforpalestinatrd"
+    },
+    ONSKELISTE: [
+        "Oppbevaringsbokser",
+        "Preseninger",
+        "Bålpanne",
+        "Gulvtepper",
+        "Varme måltider",
+        "Soveposer",
+        "Liggeunderlag",
+        "Telt",
+        "Varme klær",
+        "Hansker",
+        "Lyskilder"
+    ],
+    EVENTS: [
+        {
+          title: "Sionismen",
+          date: "14.06.2025",
+          time: "18:00",
+          location: "Thawra",
+          description: "Peder Martin forteller om sionismen og dens hvordan sionismen spilleren avgjørende rolle. Han forteller også om sin erfaring med Isreal.",
+          link: "https://www.facebook.com/p/Studenter-for-Palestina-Trondheim-100087592323689/"
+        },
+        {
+          title: "TBA",
+          date: "TBA",
+          time: "00:00",
+          location: "Thawra",
+          description: "TBA",
+          link: "/"
+        }
+      ],
+};
